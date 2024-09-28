7ef2ee95f2d030e9e1f18a21b49a9cfd2f166d7d
Leri Sinatashvili <sinatashvili.leri@outlook.com>
2024-09-28 11:23:17 +0400

    Assignment 08

--- Lesson 02 Assignments/Program.cs
+++ Lesson 02 Assignments/Program.cs
@@ -1,24 +1,37 @@
-﻿int StudentScore = 0;
-String HaveMoreProducts = "";
-int AverageScore = 0;
-int NumberOfScores = 0;
-int SumOfAllScores = 0;
+﻿Console.WriteLine("Tell me your transactions and i will tell you sum of your ballance");
+
+String TransactionType = "";
+decimal TransactionMoney = 0;
+decimal SumOfIncomes = 0;
+decimal SumOfExpenses = 0;
+decimal SumOfBalance = 0;
+String HaveMoreTransactions = "";
 
 do
 {
-    Console.WriteLine("Enter student scores and i will tell you average point");
-    StudentScore = int.Parse(Console.ReadLine());
+    Console.WriteLine("Tell me if it was Income or Expense");
+    TransactionType = Console.ReadLine();
+    Console.WriteLine("Tell me how much was it");
+    TransactionMoney = decimal.Parse(Console.ReadLine());
 
-    SumOfAllScores = SumOfAllScores + StudentScore;
-    NumberOfScores++;
 
-    AverageScore = SumOfAllScores / NumberOfScores;
+    if (TransactionType == "Income")
+    {
+        SumOfIncomes = SumOfIncomes + TransactionMoney;
+        SumOfBalance = SumOfBalance + TransactionMoney;
+    }
+    if (TransactionType == "Expense")
+    {
+        SumOfExpenses = SumOfExpenses + TransactionMoney;
+        SumOfBalance = SumOfBalance - TransactionMoney;
+    }
 
-    Console.WriteLine("Have more scores ?");
-    HaveMoreProducts = Console.ReadLine();
+    Console.WriteLine("Have more transactions");
+    HaveMoreTransactions = Console.ReadLine();
 
 }
-while (HaveMoreProducts == "yes" || HaveMoreProducts == "y");
+while (HaveMoreTransactions == "yes" || HaveMoreTransactions == "y");
 
-Console.WriteLine("Students average score is ");
-Console.WriteLine(AverageScore);
+Console.WriteLine("Sum of expenses are: " + SumOfExpenses);
+Console.WriteLine("Sum of incomes are: " + SumOfIncomes);
+Console.WriteLine("Your balance is: " + SumOfBalance);
